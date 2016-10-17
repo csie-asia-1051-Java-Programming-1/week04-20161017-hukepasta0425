fa8bde94c0356b31163f2693d9161989126aca1f
hukepasta0425 <105021031@live.asia.edu.tw>
2016-10-17 15:05:01 +0800

    10/17
    GO

--- src/ex/ex04_105021031.java
+++ src/ex/ex04_105021031.java
@@ -1,4 +1,5 @@
 ﻿package ex;
+
 /*
  * Topic: 讓使用者輸入 一個正整數 n 及一個字元符號，然後顯示出下列圖形 (例 n = 4,字元符號= @)
     @@@@
@@ -6,14 +7,22 @@
     @@  
     @
  * Date: 2016/10/17
- * Author: 1050210XX 周永振老師
+ * Author: 105021031 孟京澤
  */
+import java.util.*;
 
-public class ex04_1050210XX {
+public class ex04_105021031 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner src = new Scanner(System.in);
+		int n = src.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = n; j >=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
